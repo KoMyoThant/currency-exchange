da2ae8917766f778d772d1ad13be5063cd88ab18
Myo Thant <myothantktt@gmail.com>
2018-11-25 23:00:44 +0630

    Solving duplicate currency rate

--- app/src/main/java/com/ps/currencyexchange/data/vos/CurrencyRateVO.java
+++ app/src/main/java/com/ps/currencyexchange/data/vos/CurrencyRateVO.java
@@ -2,18 +2,22 @@ package com.ps.currencyexchange.data.vos;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
+
+import io.reactivex.annotations.NonNull;
 
 /**
  * Created by pyaesone on 11/25/18
  */
-@Entity(tableName = "currency_rate")
+@Entity(tableName = "currency_rate",indices={@Index(value="country_code", unique=true)})
 public class CurrencyRateVO {
 
     @PrimaryKey(autoGenerate = true)
     private long _id;
 
     @ColumnInfo(name = "country_code")
+    @NonNull
     private String countryCode;
 
     @ColumnInfo(name = "currency_amount")
